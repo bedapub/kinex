fb1ddf3c7d2558ece6eca2211a816abe102ce2ec
Alexandra Valeanu <valeanualexandra17@gmail.com>
2023-08-21 02:39:17 +0600

    setup info

--- setup.py
+++ setup.py
@@ -6,4 +6,21 @@ setup(
     description="A python package to compute kinase scoring and enrichment",
     py_modules=["kinex"],
     package_dir={"": "src"},
+    url="https://github.com/alexandra-valeanu/kinex",
+    author="Alexandra Valeanu",
+    author_email="valeanualexandra17@gmail.com",
+    python_requires='>=3.8',
+    install_requires = [
+        "scipy >= 1.10.0",
+        "numpy >= 1.19.5",
+        "nbformat>=4.2.0",
+        "statsmodels",
+        "plotly"
+    ],
+    extras_require = {
+        "dev": [
+            "sphinx",
+            "furo",
+        ],
+    },
 )
