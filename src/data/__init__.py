0f812fc7dfb7315e89afc1a085ada353194cd6a6
Alexandra Valeanu <valeanualexandra17@gmail.com>
2023-08-20 22:28:19 +0600

    data restructuring

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,6 +1,12 @@
 from importlib import resources
+import json
 import pandas as pd
 
 def get_pssm() -> pd.DataFrame:
     with resources.path("data", "pssm_table.csv") as df:
         return pd.read_csv(df, index_col=0)
+    
+def get_groups() -> dict:
+    with resources.path("data", "groups.json") as file_path:
+        with open(file_path) as json_file:
+            return json.load(json_file)
