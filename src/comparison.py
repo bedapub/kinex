85044c332c6d1153b996a293dc89ace96f06e362
Alexandra Valeanu <valeanualexandra17@gmail.com>
2023-10-22 21:18:41 +0600

    Major improvement Comparison class

--- src/comparison.py
+++ src/comparison.py
@@ -1,80 +1,103 @@
 import pandas as pd
 import numpy as np
-from os import listdir
 from sklearn.manifold import MDS, TSNE
 from sklearn import preprocessing
         
 import plotly.express as px
 import plotly.graph_objects as go
-from functions import get_distances, get_distance
+from functions import get_distances
+from data import get_experiments
 
 
 class Comparison:
 
-    def __init__(self, DATA_PATH: str, precomputed_distance_matrix: pd.DataFrame,) -> None:
+    def __init__(self) -> None:
+        self.experiments = get_experiments()
+    
+    def get_comparison(self, input_data: pd.DataFrame, method: str = "tSNE", center: bool = True):
 
-        # List with path to all files
-        table_paths = [f for f in listdir(DATA_PATH) if f != 'my-directory-list.csv']
-        # Read all the enrichment tables
-        tables = [pd.read_csv(f'{DATA_PATH}{file}') for file in table_paths]
+        method = method.upper()
+        supportedMethods = ["TSNE", "MDS"]
 
-        # Split table into individual tables based on the experiment
-        self.coordinates = {f'{experiment}_{dose}': table[table.experiment_name == experiment][table.dose == dose]
-                    for table in tables for experiment in table.experiment_name.unique() for dose in table[table.experiment_name == experiment].dose.unique()}
+        if method not in supportedMethods:
+            raise ValueError(
+                f"The method {method} is not supported. Supported methods: " + supportedMethods)
 
-        # Index for the plot
-        self.color_index = [f'{experiment}_{dose}' for table in tables for experiment in table.experiment_name.unique() for dose in table[table.experiment_name == experiment].dose.unique()]
-        self.text_index = [dose for table in tables for experiment in table.experiment_name.unique() for dose in table[table.experiment_name == experiment].dose.unique()]
-        self.precomputed_distance_matrix = precomputed_distance_matrix
-        self.precomputed_distance_matrix.columns = range(precomputed_distance_matrix.columns.size)
+        # Add Input and DMSO
+        nExperiments = len(self.experiments)
 
+        self.experiments.append({"id": nExperiments, "experiment_name": "DMSO", "dose": 0,
+                            "dominant_enrichment_value_log2": np.zeros(303), "dominant_p_value_log10_abs": np.zeros(303)})
 
-    def get_comparison(self, input_sample: pd.DataFrame):
-        coordinates_keys = list(self.coordinates.keys())
-        # calculate the distances from input to every exepriment
-        input_distances = pd.DataFrame([get_distances(input_sample[['dominant_enrichment_value_log2', 'dominant_p_value_log10_abs']], self.coordinates[sample][[
-                                    'dominant_enrichment_value_log2', 'dominant_p_value_log10_abs']]) for sample in coordinates_keys])
-        # Set the index
-        input_distances.index = [
-            f"{row};{len(coordinates_keys)}" for row, _ in enumerate(coordinates_keys)]
+        self.experiments.append({"id": nExperiments + 1, "experiment_name": input_data.experiment_name.unique()[0], "dose": input_data.dose.unique()[
+            0], "dominant_enrichment_value_log2": np.array(input_data["dominant_enrichment_value_log2"]), "dominant_p_value_log10_abs": np.array(input_data["dominant_p_value_log10_abs"])})
 
-        input_distances.columns = range(input_distances.columns.size)
-        
-        # Add distances to input to precomputed matrix
-        dist_matrix = pd.concat([input_distances, self.precomputed_distance_matrix])
-        # Scale the distances
-        dist_matrix = pd.DataFrame(preprocessing.scale(dist_matrix), index=dist_matrix.index)
+        # Calculate how many distances need to be computed for experiments + DMS0 + Input
+        nExperiments += 2
+        nDistances = (int)((nExperiments)*(nExperiments - 1) / 2)
 
-        # Sum the columns
-        len_keys = len(coordinates_keys) + 1
-        dist_matrix_scaled = np.zeros([len_keys, len_keys])
+        # Compute the distances
+        distancesIndex = np.empty(nDistances, dtype=tuple)
+        distances = np.empty((nDistances, 303))
 
-        for row in range(len_keys):
-            for column in range(row + 1, len_keys):
-                dist_matrix_scaled[row, column] = np.sum(
-                    dist_matrix.loc[f"{row};{column}", :])
-                dist_matrix_scaled[column, row] = np.sum(
-                    dist_matrix.loc[f"{row};{column}", :])
+        # Color and text indices
+        colorIndex = np.empty(nExperiments, dtype=object)
+        textIndex = np.empty(nExperiments, dtype=object)
 
-        if dist_matrix_scaled.min().min() < 0:
-            dist_matrix_scaled += abs(dist_matrix_scaled.min().min())
+        counter = 0
+        for id1, experiment1 in enumerate(self.experiments):
+            # Save the values for plot indexing
+            colorIndex[id1] = experiment1["experiment_name"]
+            textIndex[id1] = experiment1["dose"]
 
-        X_transform = TSNE(n_components=2, learning_rate='auto', init="random",
-                        perplexity=50, metric='precomputed', random_state=0).fit_transform(dist_matrix_scaled)
+            for experiment2 in self.experiments[id1+1:]:
+                distances[counter][:] = get_distances(
+                    experiment1=experiment1, experiment2=experiment2)
+                distancesIndex[counter] = (experiment1["id"], experiment2["id"])
+                counter += 1
 
-        fig1 = px.scatter(X_transform[:-1], x=0, y=1, color=self.color_index,
-                        hover_name=self.text_index, opacity=0.6)
+        # Scale the data
+        distances = preprocessing.scale(distances)
+
+        # Shift values to remove negative values
+        minValue = distances.min().min()
+        if minValue < 0:
+            distances -= minValue
+
+        # Create a dissimilarity matrix
+        dissimilarityMatrix = np.zeros((nExperiments, nExperiments))
+
+        for id, distance in enumerate(distances):
+            ids = distancesIndex[id]
+
+            dist = distance.sum()
+            dissimilarityMatrix[ids[0]][ids[1]] = dist
+            dissimilarityMatrix[ids[1]][ids[0]] = dist
+
+        if method == 'TSNE':
+            X_transform = TSNE(n_components=2, learning_rate='auto', init="random",
+                            perplexity=50, metric='precomputed', random_state=0).fit_transform(dissimilarityMatrix)
+        elif method == 'MDS':
+            X_transform = MDS(n_components=2, dissimilarity='precomputed', normalized_stress="auto",
+                            random_state=0).fit_transform(dissimilarityMatrix)
+
+        if center:
+            X_transform -= X_transform[-2]
+
+        fig1 = px.scatter(X_transform[:-1], x=0, y=1, color=colorIndex[:-1],
+                        hover_name=textIndex[:-1], opacity=0.6)
 
         fig1.update_traces(marker=dict(size=5, line=dict(width=0.5)),
                         textposition='top right')
 
         fig2 = px.scatter(X_transform[-1:], x=0, y=1, color_discrete_sequence=[
-                        'red'], text=["Input"])
+            'red'], text=["Input"])
         fig2.update_traces(marker=dict(size=10, line=dict(width=2)),
                         textposition='top right')
 
         fig = go.Figure(data=fig1.data + fig2.data)
-        fig.update_layout(title='t-SNE ALL DRUGS', xaxis_title=f't-SNE1', yaxis_title=f't-SNE2', template="none", showlegend=False, xaxis=dict(ticks="outside",
-                        mirror=True, showline=True), yaxis=dict(ticks="outside", mirror=True, showline=True), legend=dict(title="Drug"), width=800, height=800)
+        fig.update_layout(title=f"{method} ALL DRUGS", xaxis_title=f"{method}1", yaxis_title=f"{method}2", template="none", showlegend=False, xaxis=dict(ticks="outside",
+                                                                                                                                                        mirror=True, showline=True), yaxis=dict(ticks="outside", mirror=True, showline=True), legend=dict(title="Drug"), width=800, height=800)
         fig.update_yaxes(scaleanchor="x", scaleratio=1)
+
         return fig
