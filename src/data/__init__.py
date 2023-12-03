d0266619c6475978387d5ff5f96ced2e62016d72
Alexandra Valeanu <valeanualexandra17@gmail.com>
2023-12-04 00:29:08 +0600

    fixup! fix tests, setup for conda

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -3,15 +3,15 @@ import json
 import pandas as pd
 
 def get_pssm() -> pd.DataFrame:
-    with resources.path("data", "pssm_table.csv") as df:
+    with resources.path("kinex.data", "pssm_table.csv") as df:
         return pd.read_csv(df, index_col=0)
     
 def get_groups() -> dict:
-    with resources.path("data", "groups.json") as file_path:
+    with resources.path("kinex.data", "groups.json") as file_path:
         with open(file_path) as json_file:
             return json.load(json_file)
         
 def get_experiments() -> dict:
-    with resources.path("data", "experiments.json") as file_path:
+    with resources.path("kinex.data", "experiments.json") as file_path:
         with open(file_path) as json_file:
             return json.load(json_file)
